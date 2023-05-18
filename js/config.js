e7f30c6ae1a766834ddd0f94c6d9b2c5f0c93e70
ppayal8866 <ppayal8866@gmail.com>
2023-05-18 13:34:02 -0700

    new_config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_0f4glzt6H', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'ro7v1r4el9utmlaeb4da3nlrf', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
